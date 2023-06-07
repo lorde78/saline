730e7cfd387693f242aa866bbd30887afda92886
Kavalier.e.44 <kavalier.e.44@Kavaliere44.local>
2023-06-07 12:46:42 +0200

    fix - mise à jour de la Stack Docker sur les services Express & Nginx -> optimisation de l'environnement de dev via nondemon

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var clearModule = require('clear-module');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testRouter = require('./routes/test');
 
 var app = express();
+
+// clear the cache of required modules on server restart
+if (process.env.NODE_ENV !== 'production') {
+    clearModule.all();
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,21 +28,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
