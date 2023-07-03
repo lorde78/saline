0cd37e16e88b4ed30adacc8160d95487bca8351d
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-07-03 17:39:39 +0200

    init: création du kit sélecteur de couleur

---
+++ frontend/app/kits/color_picker.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Color_picker() {
+    return (
+        <label className={"color_picker"}>
+            <input type={"color"}/>
+            <i className="ri-pencil-line" ></i>
+        </label>
+    )
+}
