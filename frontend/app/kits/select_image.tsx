0fd033d573fe86de752a83dc14cf483128d95bfb
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-07-03 14:43:54 +0200

    init : Création du kit sélection d'image

---
+++ frontend/app/kits/select_image.tsx
@@ -0,0 +1,27 @@
+import 'remixicon/fonts/remixicon.css'
+import {useState} from "react";
+
+export default function Select_image() {
+
+    const showPreview = (input) => {
+        console.log(input);
+        var url = URL.createObjectURL(input);
+        console.log(' image : ' + url)
+        var preview = document.getElementById("image_preview");
+        preview.src = url;
+        preview.style.display = "block";
+
+    }
+
+    return (
+        <div className={"Selct_image_contaienr"}>
+            <label htmlFor="images" className="drop_container">
+                 <img id={"image_preview"} src="" />
+                <input type="file" id="images" accept="image/*" onChange={(event: object) => {
+                    showPreview(event.target.files[0])
+                }} required/>
+                <i className="ri-image-add-line"></i>
+            </label>
+        </div>
+    )
+}
