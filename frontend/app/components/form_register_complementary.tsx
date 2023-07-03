abe14a29e27003a5ed0848adfad2f101dda96f63
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-07-03 14:45:23 +0200

    Fix : Rajout du kit de sélection d'image au formulaire d'inscription

--- frontend/app/components/form_register_complementary.tsx
+++ frontend/app/components/form_register_complementary.tsx
@@ -1,11 +1,13 @@
 import Input from "~/kits/input";
 import Select from "~/kits/select";
 import Checkbox from "~/kits/checkbox";
+import Select_image from "~/kits/select_image";
 
 export default function Form_register_complementary() {
     return (
         <form action="" method="post">
             <h1>Inscription</h1>
+            <Select_image />
             <Select id={"Gender"} name={"Gender"} contents={[{value:"Men", option:"Homme"},{value:"Woman", option:"Femme"}, {value:"Other", option:"Autre"}]} />
             <Input name={"BirthDate"} type={"date"} placeholder={"Date de naissance"} />
             <Input name={"Country"} type={"text"} placeholder={"Pays"} />
