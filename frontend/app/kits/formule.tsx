5412efe851c27b4f2857c68602d88e654650ba9d
Wylohn <wylohn.tribout@gmail.com>
2023-07-03 17:19:47 +0200

    init: formule

---
+++ frontend/app/kits/formule.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "~/style/formule.css";
+
+type FormuleProps = {
+  subscription: string;
+};
+
+const Formule: React.FC<FormuleProps> = ({ subscription }) => {
+
+  return (
+    <div className="formule">
+
+      <div className="formule_infos">
+        <p className="formule_title">Votre formule</p>
+
+        <div className="formule_choice">
+          <div className="formule_sub">
+            <p>{subscription}</p>
+          </div>
+          <div className="formule_price">
+            <p className="formule_price_value">9.9€/Mois</p>
+            <p className="formule_price_total">Paiement annuel de 118.8€</p>
+          </div>
+        </div>
+        
+        <p className="formule_edit-sub">Modifier mon abonnement</p>
+      </div>
+      
+      
+    </div>
+  );
+};
+
+export default Formule;
