47f4b989c8062093c246d3959838e18efbfb099d
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-07-04 15:49:06 +0200

    init : creation du kit Builder block button

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,7 +1,10 @@
 import type { V2_MetaFunction } from "@remix-run/node";
 import Footer from "~/components/footer";
 import Header from "~/components/header";
-
+import Builder_block_button from "~/kits/builder_block_button";
+import "~/styles/reset.css"
+import "~/styles/style.css"
+import "~/styles/builder.css"
 export const meta: V2_MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -9,12 +12,12 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+
 export default function Index() {
   console.log("Index")
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <Header title={""}/>
-      <Footer />
+      <Builder_block_button  contents={"Vidéo"} icon={"ri-vidicon-line"}/>
     </div>
   );
 }
