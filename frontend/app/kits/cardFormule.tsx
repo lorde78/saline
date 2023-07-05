46baa16cbc073986e160fc0c47225f7cd6c4a4ca
Wylohn <wylohn.tribout@gmail.com>
2023-07-05 15:35:38 +0200

    init: carte de formule

---
+++ frontend/app/kits/cardFormule.tsx
@@ -0,0 +1,19 @@
+import "~/styles/cardFormule.css";
+
+type CardFormuleProps = {
+
+};
+
+const CardFormule: React.FC<CardFormuleProps> = ({  }) => {
+  
+  return (
+    <div>
+        <div className='card_formule'>
+            
+        </div>
+
+    </div>
+  );
+};
+
+export default CardFormule;
--- frontend/app/kits/formule.tsx
+++ frontend/app/kits/formule.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import FormuleTag from "./formuleTag";
 
 type FormuleProps = {
