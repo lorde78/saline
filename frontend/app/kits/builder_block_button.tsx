e0206867b139fa4f0e89fc7627074fa06d8198c9
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-07-05 13:53:23 +0200

    format :  correction du kit builder_block_button

--- frontend/app/kits/builder_block_button.tsx
+++ frontend/app/kits/builder_block_button.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 export default function Builder_block_button({icon, contents}: Props) {
     return (
-        <div className={"builder_block_button"}>
+        <button className={"button builder_block_button"}>
             <p>{contents}</p>
             <i className={ icon }></i>
-        </div>
+        </button>
     )
 }
