532d97e592c177dd45aec1c1d4d0d8f4ccc26e84
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-07-06 16:09:20 +0200

    format: suppression des console.log et correction de l'import du style

--- frontend/app/routes/cours.edit.tsx
+++ frontend/app/routes/cours.edit.tsx
@@ -1,11 +1,14 @@
 import type {V2_MetaFunction} from "@remix-run/node";
 import Builder_navigation from "~/components/builder_navigation";
+import Builder_step_review from "~/components/builder_step_review";
+
 import Builder_select_step from "~/components/builder_select_step";
 
-import "~/styles/reset.css"
-import "~/styles/style.css"
-import "~/styles/input.css"
-import "~/styles/builder.css"
+import resetStyles from "~/styles/reset.css";
+import styles from "~/styles/style.css";
+import input from "~/styles/input.css";
+import builder from "~/styles/builder.css";
+import Builder_step_video from "~/components/builder_step_video";
 
 
 export const meta: V2_MetaFunction = () => {
@@ -15,12 +18,20 @@ export const meta: V2_MetaFunction = () => {
     ];
 };
 
+export function links() {
+    return [
+        {rel: 'stylesheet', href: resetStyles},
+        {rel: 'stylesheet', href: styles},
+        {rel:'stylesheet', href: input},
+        {rel: 'stylesheet', href: builder}
+    ]
+}
 
 export default function CoursEdit() {
     return (
         <div className={"builder_container"}>
             <Builder_navigation />
-            <Builder_select_step />
+            <Builder_step_video />
         </div>
     );
 }
