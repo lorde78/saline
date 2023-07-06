8bb714ba6433318a46971beda296365675c4d830
Wylohn <wylohn.tribout@gmail.com>
2023-07-06 17:30:22 +0200

    fonc: page modification de formule + card

--- frontend/app/components/editFormule.tsx
+++ frontend/app/components/editFormule.tsx
@@ -1,6 +1,7 @@
 import React  from 'react';
 import FormuleTag from '~/kits/formuleTag';
 import HeaderNav from "~/kits/headerNav";
+import CardFormule from '~/kits/cardFormule';
 import "~/styles/editFormule.css";
 
 type EditFormuleProps = {
@@ -13,11 +14,19 @@ const EditFormule: React.FC<EditFormuleProps> = ({  }) => {
     <div>
         <HeaderNav namePage="Modifier mon abonnement"/>
         <div className='edit_formule'>
+          <div>
             <FormuleTag subscription='Annuel'/>
+            <div className='price-formule'>
+              <p className="formule_price_value">9.9€/Mois</p>
+              <p className="formule_price_total">Paiement annuel de 118.8€</p>
+            </div>
+          </div>
+          <button className='unsub-button'>Résilier</button>
         </div>
 
         <div className="all-formule">
-            <p>Tous les abonnements</p>
+            <h2>Tous les abonnements</h2>
+            <CardFormule sub='Mensuel' price={19.8} buttonName='Modifier'/>
         </div>
 
 
--- frontend/app/kits/cardFormule.tsx
+++ frontend/app/kits/cardFormule.tsx
@@ -3,9 +3,10 @@ import "~/styles/cardFormule.css";
 type CardFormuleProps = {
   sub: string;
   price: number;
+  buttonName: string;
 };
 
-const CardFormule: React.FC<CardFormuleProps> = ({ sub, price, }) => {
+const CardFormule: React.FC<CardFormuleProps> = ({ sub, price, buttonName}) => {
   
   return (
     <div>
@@ -13,12 +14,28 @@ const CardFormule: React.FC<CardFormuleProps> = ({ sub, price, }) => {
             <p className="subscription-title">{sub}</p>
             <p className="subscription-price">{price}€/Mois</p>
             <div className="subscription-advantage">
-              <p>Accès illimité à toutes nos masterclasses.</p>
-              <p>De nouvelles vidéos sont disponibles chaque mois.</p>
-              <p>Des interviews exclusives avec les plus grands professeurs du monde.</p>
-              <p>Partitions annotées par nos professeurs et prêtes à être téléchargées.</p>
-              <p>Vidéos multi-angles disponibles en HD sur tous vos appareils.</p>
+                <div className="subscription-advantage-line">
+                    <i className="ri-check-line icon-check"></i>
+                    <p>Accès illimité à toutes nos masterclasses.</p>
+                </div>
+                <div className="subscription-advantage-line">
+                    <i className="ri-check-line icon-check"></i>
+                    <p>De nouvelles vidéos sont disponibles chaque mois.</p>
+                </div>
+                <div className="subscription-advantage-line">
+                    <i className="ri-check-line icon-check"></i>
+                    <p>Des interviews exclusives avec les plus grands professeurs du monde.</p>
+                </div>
+                <div className="subscription-advantage-line">
+                    <i className="ri-check-line icon-check"></i>
+                    <p>Partitions annotées par nos professeurs et prêtes à être téléchargées.</p>
+                </div>
+                <div className="subscription-advantage-line">
+                    <i className="ri-check-line icon-check"></i>
+                    <p>Vidéos multi-angles disponibles en HD sur tous vos appareils.</p>
+                </div>
             </div>
+            <button className="button">{buttonName}</button>
         </div>
 
     </div>
