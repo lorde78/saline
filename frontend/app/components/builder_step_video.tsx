f0db12fa0e4fb337f89661c3cbf4f7295844b057
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-07-06 16:16:06 +0200

    init : création du component pour l'étape vidéo

---
+++ frontend/app/components/builder_step_video.tsx
@@ -0,0 +1,9 @@
+import Builder_select_video from "~/kits/builder_select_video";
+
+export default function Builder_step_video() {
+    return (
+        <section className={"builder_step_container"}>
+            <Builder_select_video />
+        </section>
+    )
+}
