8aa540c9cd577d810387411bc9ea5d5773993a63
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-07-10 12:36:17 +0200

    fix : Correction des problème sur les kits Checkbox, input et textarea

--- frontend/app/kits/input.tsx
+++ frontend/app/kits/input.tsx
@@ -6,11 +6,44 @@ type Props = {
     type: string
     placeholder: string
     setValue: any
+    propsSetValue : any
     value: any
 };
 
+/***
+ *
+ * @param type
+ * @param name
+ * @param placeholder
+ * @param setValue
+ * @param propsSetValue (laisser vide si setValue n'est pas une fonction)
+ * @param value
+ * @constructor
+ *
+ * exemple setValue est une fonction
+ * <Input
+ *         name={"nom"}
+ *         type={"text"}
+ *         placeholder={"Nom"}
+ *         setValue={setValue}
+ *         propsSetValue{"," + props}
+ *         value={value}
+ *  />
+ *
+ * exemple setValue est un useState
+ * <Input
+ *         name={"nom"}
+ *         type={"text"}
+ *         placeholder={"Nom"}
+ *         setValue={setValue}
+ *         propsSetValue{""}
+ *         value={value}
+ *  />
+ *
+ */
 
-export default function Input({type, name, placeholder, setValue, value}: Props) {
+
+export default function Input({type, name, placeholder, setValue, propsSetValue, value}: Props) {
 
     const [typeInput, setTypeInput] = useState(type)
     const [eye, setEye] = useState("ri-eye-2-line")
@@ -33,9 +66,7 @@ export default function Input({type, name, placeholder, setValue, value}: Props)
 
     return (
         <div className="input-container">
-            <input onChange={(e) => {setValue(e.target.value)}} id={name} className={"input"} type={typeInput} placeholder=" ">
-                {value}
-            </input>
+            <input onChange={(e) => {setValue(e.target.value, propsSetValue)}} id={name} className={"input"} type={typeInput} placeholder=" " value={value} />
             <label htmlFor={name} className="placeholder">{placeholder}</label>
             {type == 'password' ? <i className={eye} onClick={showPassword}></i> : ''}
         </div>
