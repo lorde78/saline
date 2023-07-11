7ac97b3716026541ba7fbca6cebab55ee1017158
Wylohn <wylohn.tribout@gmail.com>
2023-07-11 22:50:30 +0200

    init: page professeur

---
+++ frontend/app/kits/professorInfos.tsx
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import "~/styles/professorInfos.css";
+import Tag from "./tag";
+
+type ProfessorInfosProps = {
+    src: string;
+  };
+  
+  const ProfessorInfos: React.FC<ProfessorInfosProps> = ({ src }) => {
+
+
+    return (
+        <div>
+            <div className="professor-infos_container">
+                <figure className="profile-pic">
+                    <img src={src} alt="photo de profile" />
+                </figure>
+
+                <div className="professor-role">
+                    <p>Rôle:</p>
+                    <Tag role="Professeur"/>
+                </div>
+
+                <div className="professor-instrument">
+                    <p>Instrument:</p>
+                    <Tag role="Piano"/>
+                </div>
+            </div>
+            <p>In 1990, he won the First Prize at the Geneva Competition.</p>
+        </div>
+    );
+  };
+  
+  export default ProfessorInfos;
+  
