dbaa6c3906a9c6c1b081bb8cc83d6a6e2141fe65
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-07-11 16:38:48 +0200

    fix : misse à jours des inputs dans les forms

--- frontend/app/components/form_register.tsx
+++ frontend/app/components/form_register.tsx
@@ -1,14 +1,30 @@
 import Input from "~/kits/input";
+import {NavLink} from "@remix-run/react";
+import {useState} from "react";
 
 export default function Form_register() {
+
+    const [email, setEmail] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [password, setPassword] = useState("")
+
     return (
         <form action="" method="post">
             <h1>Inscription</h1>
-            <Input type={"email"} name={"email"} placeholder={"Mail"} />
-            <Input type={"text"} name={"firstname"} placeholder={"Prénom"} />
-            <Input type={"text"} name={"lastname"} placeholder={"Nom"} />
-            <Input type={"password"} name={"password"} placeholder={"Mot de passe"} />
-            <button className={"button"} type="submit">Suivant</button>
+            <Input name={"email"} type={"email"} placeholder={"Mail"}
+                   setValue={setEmail} propsSetValue={""} value={email}/>
+
+            <Input name={"firstname"} type={"text"} placeholder={"Prénom"}
+                   setValue={setFirstName} propsSetValue={""} value={firstName}/>
+
+            <Input name={"lastname"} type={"text"} placeholder={"Nom"}
+                   setValue={setLastName} propsSetValue={""} value={lastName}/>
+
+            <Input name={"password"} type={"password"} placeholder={"Mot de passe"}
+                   setValue={setPassword} propsSetValue={""} value={password}/>
+
+            <NavLink className={"button"} type="submit" to={"/authentication/register/complementary"}>Suivant</NavLink>
             <a href={""} className={"sub_link"}>Voir la Politique confidentialité</a>
         </form>
     )
