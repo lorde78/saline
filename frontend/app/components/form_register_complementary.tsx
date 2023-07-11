61214da6156cbe90dc6d8947ec5f6f2237e2d9f3
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-07-11 16:38:48 +0200

    fix : misse à jours des inputs dans les forms

--- frontend/app/components/form_register_complementary.tsx
+++ frontend/app/components/form_register_complementary.tsx
@@ -2,18 +2,34 @@ import Input from "~/kits/input";
 import Select from "~/kits/select";
 import Checkbox from "~/kits/checkbox";
 import Select_image from "~/kits/select_image";
+import {useState} from "react";
 
 export default function Form_register_complementary() {
+    const [birthDate, setBirthDate] = useState("")
+    const [country, setCountry] = useState("")
+    const [address, setAddress] = useState("")
+    const [postalCode, setPostalCode] = useState("")
+    const [privacy, setPrivacy] = useState(false)
+
     return (
         <form action="" method="post">
             <h1>Inscription</h1>
             <Select_image />
             <Select defaultContent={"Quel est ton genre ?"} contents={[{value:"Men", option:"Homme"},{value:"Woman", option:"Femme"}, {value:"Other", option:"Autre"}]} />
-            <Input name={"BirthDate"} type={"date"} placeholder={"Date de naissance"} />
-            <Input name={"Country"} type={"text"} placeholder={"Pays"} />
-            <Input name={"Address"} type={"text"} placeholder={"Adresse"} />
-            <Input name={"PostalCode"} type={"text"} placeholder={"Code postal"} />
-            <Checkbox text={"J’ai lu et j’accepte la Politique de confidentialité"} />
+            <Input name={"BirthDate"} type={"date"} placeholder={"Date de naissance"}
+                   setValue={setBirthDate} propsSetValue={""} value={birthDate}/>
+            <Input name={"Country"} type={"text"} placeholder={"Pays"}
+                   setValue={country} propsSetValue={""} value={setCountry}/>
+            <Input name={"Address"} type={"text"} placeholder={"Adresse"}
+                   setValue={setAddress} propsSetValue={""} value={address}/>
+            <Input name={"PostalCode"} type={"text"} placeholder={"Code postal"}
+                   setValue={setPostalCode} propsSetValue={""} value={postalCode}/>
+            <Checkbox name={"privacy"}
+                      type={"checkbox"}
+                      text={"J’ai lu et j’accepte la Politique de confidentialité"}
+                      setValue={setPrivacy}
+                      propsSetValue={""} value={privacy}/>
+
             <button className={"button"} type="submit">Inscription</button>
         </form>
     )
