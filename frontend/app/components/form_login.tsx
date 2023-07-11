fd7a576b40f5710d00f574751fc901868189aa9d
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-07-11 16:38:48 +0200

    fix : misse à jours des inputs dans les forms

--- frontend/app/components/form_login.tsx
+++ frontend/app/components/form_login.tsx
@@ -1,11 +1,18 @@
 import Input from "~/kits/input";
+import {useState} from "react";
 
 export default function Form_login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+
     return (
         <form action="" method="post">
             <h1>Connexion</h1>
-            <Input type={"email"} name={"email"} placeholder={"Mail"} />
-            <Input type={"password"} name={"password"} placeholder={"Mot de passe"} />
+            <Input name={"email"} type={"email"} placeholder={"Mail"}
+                   setValue={setEmail} propsSetValue={""} value={email}/>
+            <Input name={"password"} type={"password"} placeholder={"Mot de passe"}
+                   setValue={setPassword} propsSetValue={""} value={password}/>
             <button className={"button"} type="submit">Connexion</button>
             <a href={""} className={"sub_link"}>Tu as oublié ton mot de passe ?</a>
         </form>
