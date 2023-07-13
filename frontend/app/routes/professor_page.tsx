d83ce33faf7483e253f1125d8c3d248fb5a80201
Wylohn <wylohn.tribout@gmail.com>
2023-07-13 11:04:41 +0200

    fonc: inté page professeur

--- frontend/app/routes/professor_page.tsx
+++ frontend/app/routes/professor_page.tsx
@@ -4,7 +4,7 @@ import Formule from "~/kits/formule";
 import UserInfos from "~/kits/userInfos";
 import resetStyles from "~/styles/reset.css";
 import globalStyles from "~/styles/style.css";
-import profileStyles from "~/styles/profileStyle.css";
+import professorStyles from "~/styles/professorPage.css";
 import EditUserProfile from "~/components/editUserProfile";
 import Form_login from "~/components/form_login";
 import Form_register from "~/components/form_register";
@@ -14,21 +14,22 @@ import EditPassword from "~/components/editPassword";
 import EditFormule from '~/components/editFormule';
 import formuleStyles from "~/styles/formule.css";
 import ProfessorInfos from '~/kits/professorInfos';
+import Professor_profile from '~/components/professorProfile';
+import SearchProfessor from '~/components/searchProfessor';
 
 
 
 
 
 export function links() {
-  return [{rel: 'stylesheet', href: resetStyles}, {rel: 'stylesheet', href: profileStyles}, {rel: 'stylesheet', href: globalStyles}, {rel: 'stylesheet', href: inputStyles}, {rel: 'stylesheet', href: formuleStyles}]
+  return [{rel: 'stylesheet', href: resetStyles}, {rel: 'stylesheet', href: professorStyles}, {rel: 'stylesheet', href: globalStyles}, {rel: 'stylesheet', href: inputStyles}, {rel: 'stylesheet', href: formuleStyles}]
 }
 
 export default function Professor_page() {
     return (
-        <div className="professor-page">
-            <ProfessorInfos src='/assets/images/1000x1500-pour-site14.png'/>
-            <Accordion title="Les formations" content="Content 1" picto="ri-book-mark-line" />
-            <Accordion title="Les interviews" content="Content 2" picto="ri-book-mark-line" />
+        <div className="professor-profile">
+            {/* <Professor_profile /> */}
+            <SearchProfessor />
         </div>
     )
 }
