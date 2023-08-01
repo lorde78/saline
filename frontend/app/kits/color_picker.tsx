9d6d951d0d0e395c62706b6460092bd79c738e1b
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-08-01 16:58:06 +0200

    fix : Récupération de la data dans l'éditeur de bannières

--- frontend/app/kits/color_picker.tsx
+++ frontend/app/kits/color_picker.tsx
@@ -1,14 +1,17 @@
 type Props = {
     setValue: any
     value: any
+    propsSetValue: any
 };
 
-export default function Color_picker({value, setValue}: Props) {
+export default function Color_picker({value, setValue, propsSetValue}: Props) {
     return (
         <label className={"color_picker"}>
             <input type={"color"} value={value}
-                   onChange={(e) => {setValue(e.target.value)}}/>
-            <i className="ri-pencil-line" ></i>
+                   onChange={(e) => {
+                       setValue(e.target.value, propsSetValue)
+                   }}/>
+            <i className="ri-pencil-line"></i>
         </label>
     )
 }
