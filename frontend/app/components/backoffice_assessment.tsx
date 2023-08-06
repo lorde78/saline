c67a401d39ce85afbe71525f3d1d464a14eec877
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-08-06 12:33:09 +0200

    Fix: correction de l'affichage des évalutations

--- frontend/app/components/backoffice_assessment.tsx
+++ frontend/app/components/backoffice_assessment.tsx
@@ -5,36 +5,20 @@ import Input from "~/kits/input";
 import Color_picker from "~/kits/color_picker";
 import Checkbox from "~/kits/checkbox";
 import Textarea from "~/kits/textarea";
+import {NavLink, Outlet} from "@remix-run/react";
 
 type Props = {
     id: number,
     studen: string,
     course: string,
-    evaluationType: string
-    contente: string
-    note: string
-    isValidate: boolean
-    isNotValidate: boolean
-    ratragage: boolean
-    noRatragage: boolean
     status: string
-    setValue: any
 };
 export default function Backoffice_assessment({
                                                   id,
                                                   studen,
                                                   course,
-                                                  evaluationType,
-                                                  contente,
-                                                  note,
-                                                  isValidate,
-                                                  isNotValidate,
-                                                  ratragage,
-                                                  noRatragage,
                                                   status,
-                                                  setValue
                                               }: Props) {
-    const [editing, setEditing] = useState(false)
 
     const statusColor = () => {
         switch (status) {
@@ -56,119 +40,19 @@ export default function Backoffice_assessment({
         }
     }
 
-    const displayContent = () => {
-        switch (evaluationType) {
-            case "video":
-                return (
-                    <iframe
-                        className={"content"}
-                        src={contente}
-                        title="YouTube video player"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowFullScreen>
-                    </iframe>
-                )
-        }
-    }
     return (
         <>
-            {editing ?
-                <div className={"assessment_container-open"}>
-                    <h1>Contenu : {studen}</h1>
-                    <h3>{course}</h3>
-                    {displayContent()}
-                    <div className={"assessment_section"}>
-                        <p>Valider</p>
-                        <div>
-                            <span>
-                                <p>Oui</p>
-                                <Checkbox
-                                    name={"checkYes"}
-                                    type={"checkbox"}
-                                    text={""}
-                                    setValue={setValue}
-                                    propsSetValue={{id: id, valuToChange: "isValidate"}}
-                                    value={isValidate}
-                                />
-                            </span>
-                            <span>
-                                <p>Nom</p>
-                                <Checkbox
-                                    name={"checkNo"}
-                                    type={"checkbox"}
-                                    text={""}
-                                    setValue={setValue}
-                                    propsSetValue={{id: id, valuToChange: "isNotValidate"}}
-                                    value={isNotValidate}
-                                />
-                            </span>
-                        </div>
-                    </div>
-                    {isNotValidate ?
 
-                        <div className={"assessment_section"}>
-                            <p>Ratrapage</p>
-                            <div>
-                            <span>
-                                <p>Oui</p>
-                                <Checkbox
-                                    name={"checkYes"}
-                                    type={"checkbox"}
-                                    text={""}
-                                    setValue={setValue}
-                                    propsSetValue={{id: id, valuToChange: "ratragage"}}
-                                    value={ratragage}
-                                />
-                            </span>
-                                <span>
-                                <p>Nom</p>
-                                <Checkbox
-                                    name={"checkNo"}
-                                    type={"checkbox"}
-                                    text={""}
-                                    setValue={setValue}
-                                    propsSetValue={{id: id, valuToChange: "noRatragage"}}
-                                    value={noRatragage}
-                                />
-                            </span>
-                            </div>
-                        </div>
-                        :
-                        ""
-                    }
-                    <div className={"assessment_section"}>
-                        <Textarea
-                            name={"note"}
-                            placeholder={"Remarque"}
-                            setValue={setValue}
-                            propsSetValue={{id: id, valuToChange: "note"}}
-                            value={note}
-                        />
-                    </div>
-
-                    <button className={"button"} onClick={() => {
-                        setEditing(false)
-                    }}>Valider
-                    </button>
+            <NavLink className={"assessment_container"} to={"/backoffice/assessments/" + id}>
+                <div className={"assessment_container-left"}>
+                    <p>{studen}</p>
+                    <p>{course}</p>
                 </div>
-                :
-
-                <div className={"assessment_container"}>
-                    <div onClick={() => {
-                        setEditing(true)
-                    }}>
-                        <div className={"assessment_container-left"}>
-                            <p>{studen}</p>
-                            <p>{course}</p>
-                        </div>
-                        <div className={"assessment_container-right"}>
-                            <p>{status}</p>
-                            {statusColor()}
-                        </div>
-                    </div>
-                    <button className={"button"}>Envoyer</button>
+                <div className={"assessment_container-right"}>
+                    <p>{status}</p>
+                    {statusColor()}
                 </div>
-            }
+            </NavLink>
         </>
     );
 }
