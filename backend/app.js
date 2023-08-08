17b7e04fe302af06d926a5749d4e7ff4713de5d8
Bentani Amin <amin.bentani@hetic.net>
2023-08-08 00:46:48 +0000

    Authentification

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 var clearModule = require('clear-module');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testRouter = require('./routes/test');
+var registerRouter = require('./routes/authentication/register.routes.ts');
+var loginRouter = require('./routes/authentication/login.routes.ts');
 
 var app = express();
 
@@ -27,8 +27,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/test', testRouter);
+app.use('/register', registerRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
