2f77a1f431884d61bdc1c6678b7524f15085daab
Bentani <amin.bentani@hetic.net>
2023-08-08 13:40:33 +0200

    fix: relations modele

---
+++ backend/prisma/migrations/20230808112552_/migration.sql
@@ -0,0 +1,8 @@
+-- DropForeignKey
+ALTER TABLE `comment` DROP FOREIGN KEY `Comment_id_fkey`;
+
+-- AlterTable
+ALTER TABLE `comment` ADD COLUMN `userId` VARCHAR(191) NULL;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
