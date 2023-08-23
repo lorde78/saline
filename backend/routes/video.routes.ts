3f7690e1fb36244b9bd0d5cab4014d2dfbc6517b
Bentani <amin.bentani@hetic.net>
2023-08-23 23:01:21 +0200

    init: routes video

---
+++ backend/routes/video.routes.ts
@@ -0,0 +1,53 @@
+var express = require('express');
+const { database } = require('../config/db.ts');
+
+var router = express.Router();
+
+router.post('/', async function (req, res, next) {
+    const { title, url, lessonsId } = req.body;
+    const video = await database.video.create({
+        data: {
+            title: title,
+            url: url,
+            lessonsId: lessonsId,
+        }
+    })
+
+    res.json({
+        message: 'video added',
+    });
+
+});
+
+router.delete('/', async function (req, res, next) {
+    const { id } = req.query;
+    const deletevideo = await database.video.delete({
+        where: {
+            id: id,
+        },
+    })
+    res.json({
+        message: 'video deleted',
+    });
+});
+
+router.get('/', async function (req, res, next) {
+    const { id, lessonId } = req.query;
+    if (!id || !lessonId) {
+        res.status(400);
+        throw new Error('You must provide an id or lessonId. ');
+    }
+    const videos = await database.video.findMany({
+        where: {
+            OR: [
+                { videoId: id },
+                { lessonId: id },
+              ],
+          },
+    })
+    res.json({
+        "videos": videos
+    });
+});
+
+module.exports = router;
