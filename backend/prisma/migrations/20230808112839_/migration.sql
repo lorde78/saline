c320a730ff11343c535bae16ce5dc19ceb7f0588
ArkhayDs <raphael.ds.tic@gmail.com>
2023-08-26 16:42:45 +0200

    fonc: ajout d'un service au Docker compose avec mysql. Mise à jour de la config Prisma pour prendre compte du service docker

--- backend/prisma/migrations/20230808112839_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AddForeignKey
-ALTER TABLE `Answer` ADD CONSTRAINT `Answer_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `Answer` ADD CONSTRAINT `Answer_commentId_fkey` FOREIGN KEY (`commentId`) REFERENCES `Comment`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
