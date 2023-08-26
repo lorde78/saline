e3a9ba3779d685881f734c9c7467bf3c3a83a015
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-08-26 14:18:28 +0200

    Fix : mise à jour du builder (rajout de la navigation)

--- frontend/app/components/builder_select_step.tsx
+++ frontend/app/components/builder_select_step.tsx
@@ -1,12 +1,14 @@
 import Builder_block_button from "~/kits/builder_block_button";
-
-export default function Builder_select_step() {
+interface Props {
+    setValue: any
+}
+export default function Builder_select_step({setValue}: Props) {
     return (
         <section className={"builder_step_container"}>
             <nav className={"builder_select_step_nav"}>
-                <Builder_block_button icon={"ri-vidicon-line"} contents={"Vidéo"} />
-                <Builder_block_button icon={"ri-file-edit-line"} contents={"Exercice"} />
-                <Builder_block_button icon={"ri-award-line"} contents={"Examen"} />
+                <Builder_block_button icon={"ri-vidicon-line"} contents={"Vidéo"} setValue={setValue} props={"video"} />
+                <Builder_block_button icon={"ri-file-edit-line"} contents={"Exercice"} setValue={setValue} props={"exercise/qcm"} />
+                <Builder_block_button icon={"ri-award-line"} contents={"Examen"} setValue={setValue} props={"review"} />
             </nav>
         </section>
     )
