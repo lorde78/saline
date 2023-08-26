b8675df22451e2e73ea78ffa924e6768b53cdb55
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-08-26 16:56:34 +0200

    format : retouche du bouton examen off

--- frontend/app/kits/builder_block_button.tsx
+++ frontend/app/kits/builder_block_button.tsx
@@ -11,9 +11,8 @@ interface Props {
 export default function Builder_block_button({icon, contents, setValue, props, off}: Props) {
     return (
         <button
-            className={off ? "builder_block_button" : "button builder_block_button"}
+            className={off ? "builder_block_button builder_block_button-off" : "button builder_block_button"}
             onClick={off ? () => console.log("button is off") : () => setValue(props)}
-            style={off ? {opacity: "50%"} : {}}
         >
             <p>{contents}</p>
             <i className={icon}></i>
