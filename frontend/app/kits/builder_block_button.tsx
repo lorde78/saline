d134fe36b767a7e8fd40125b2040c30a64073049
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-08-26 16:50:34 +0200

    fonc : transfer de la data dans le builder

--- frontend/app/kits/builder_block_button.tsx
+++ frontend/app/kits/builder_block_button.tsx
@@ -1,16 +1,22 @@
 import 'remixicon/fonts/remixicon.css'
+
 interface Props {
     icon: string
     contents: string
     setValue: any
     props: any
+    off: boolean
 }
 
-export default function Builder_block_button({icon, contents, setValue, props}: Props) {
+export default function Builder_block_button({icon, contents, setValue, props, off}: Props) {
     return (
-        <button className={"button builder_block_button"} onClick={() => setValue(props)}>
+        <button
+            className={off ? "builder_block_button" : "button builder_block_button"}
+            onClick={off ? () => console.log("button is off") : () => setValue(props)}
+            style={off ? {opacity: "50%"} : {}}
+        >
             <p>{contents}</p>
-            <i className={ icon }></i>
+            <i className={icon}></i>
         </button>
     )
 }
