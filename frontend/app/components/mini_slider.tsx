d7c0fcd3efc09f19cbdef3a5d0b1d66dc1b382b1
lorde78 <basil.lombard@hetic.net>
2023-08-31 15:34:33 +0200

    update : correctif graphic de la page dacceuil

---
+++ frontend/app/components/mini_slider.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import "swiper/swiper-bundle.min.css";
+import '../styles/mini_slider.css';
+import '../styles/style.css';
+
+interface Slide {
+    src: string;
+    title: string;
+    name: string;
+    desc: string;
+}
+
+interface SliderProps {
+    slides: Slide[];
+}
+
+export const MiniSlider: React.FunctionComponent<SliderProps> = ({ slides }) => {
+    const [Swiper, setSwiper] = React.useState<any>(null);
+
+    React.useEffect(() => {
+        import("swiper/react").then((module) => {
+            setSwiper(module);
+        });
+    }, []);
+
+    if (!Swiper) {
+        return null;
+    }
+
+    const { Swiper: SwiperComponent, SwiperSlide } = Swiper;
+
+    const defaultSlides: Slide[] = [];
+
+    const allSlides = slides.length > 0 ? slides : defaultSlides;
+
+    return (
+        <div className="mini_swiper_container">
+            <h2>Progressez aux côtés des plus grands professeurs.</h2>
+            <SwiperComponent
+                slidesPerView={2}
+                spaceBetween={20}
+            >
+                {allSlides.map((slide, index) => (
+                    <SwiperSlide key={index}>
+                        <img src={slide.src} alt={slide.title} />
+                        <div className="custom_text_socket_container">
+                            <h3>{slide.name}</h3>
+                            <span>{slide.desc}</span>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </SwiperComponent>
+        </div>
+    );
+};
