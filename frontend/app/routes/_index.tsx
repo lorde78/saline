5950f040e0995cb2f849a71d324601bf2d2845c5
lorde78 <basil.lombard@hetic.net>
2023-08-31 15:34:33 +0200

    update : correctif graphic de la page dacceuil

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -2,16 +2,14 @@ import type {V2_MetaFunction} from "@remix-run/node";
 import Footer from "~/components/footer";
 import Header from "~/components/header";
 import { Slider } from "~/components/slider";
+import { MiniSlider } from "~/components/mini_slider";
 import { SectionInstrument } from "~/components/section_instrument";
 import { SectionPartenaires } from "~/components/section_partenaires";
 import { SectionSalinePlus } from "~/components/section_plus_saline";
-import Builder_block_button from "~/kits/builder_block_button";
 import resetStyles from "~/styles/reset.css"
 import styles from "~/styles/style.css"
 import builder from "~/styles/builder.css"
 import input from "~/styles/input.css"
-import Builder_select_folder from "~/kits/builder_select_folder";
-import Select_image from "~/kits/select_image";
 
 export const meta: V2_MetaFunction = () => {
     return [
@@ -34,26 +32,38 @@ export default function Index() {
     {
       src: "/assets/images/first.png",
       title: "Image 1",
+      name: "first guy",
+      desc: "La la la la ala aalazifjfijf"
     },
     {
       src: "assets/images/1000x1500-pour-site14.png",
       title: "Image 2",
+      name: "first guy",
+      desc: "La la la la ala aalazifjfijf"
     },
     {
       src: "assets/images/1000x1500-pour-site18.png",
       title: "Image 3",
+      name: "first guy",
+      desc: "La la la la ala aalazifjfijf"
     },
     {
       src: "assets/images/1000x1500-pour-site22.png",
       title: "Image 4",
+      name: "first guy",
+      desc: "La la la la ala aalazifjfijf"
     },
     {
       src: "assets/images/1000x1500-pour-site23.png",
       title: "Image 5",
+      name: "first guy",
+      desc: "La la la la ala aalazifjfijf"
     },
     {
       src: "assets/images/1000x1500-pour-site48.png",
       title: "Image 6",
+      name: "first guy",
+      desc: "La la la la ala aalazifjfijf"
     },
 
   ];
@@ -64,8 +74,9 @@ export default function Index() {
       <Header title={""} />
       <Slider slides={slides} />
       <SectionInstrument />
-      <SectionPartenaires />
+      <MiniSlider slides={slides}/>
       <SectionSalinePlus />
+      <SectionPartenaires />
       <Footer />
     </div>
   );
