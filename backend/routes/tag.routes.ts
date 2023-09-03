055aaa82d28753d3c1970ae2deba61e4b3ce0211
bentaniamin1 <amin.bentani@hetic.net>
2023-09-03 13:03:49 +0200

    fonc: méthodes crud  routes tag

---
+++ backend/routes/tag.routes.ts
@@ -0,0 +1,68 @@
+var express = require('express');
+const { database } = require('../config/db.ts');
+
+var router = express.Router();
+
+router.post('/', async function (req, res, next) {
+    const { title } = req.body;
+    const tag = await database.tag.create({
+        data: {
+            title: title,
+        }
+    })
+
+    res.json({
+        message: 'tag added',
+    });
+
+});
+
+router.delete('/', async function (req, res, next) {
+    const { id } = req.query;
+    const deletetag = await database.tag.delete({
+        where: {
+            id: id,
+        },
+    })
+    res.json({
+        message: 'tag deleted',
+    });
+});
+
+router.put('/', async function (req, res, next) {
+    const { id } = req.query;
+
+    if (!id) {
+        res.status(400);
+        throw new Error('You must provide an id or tagId.');
+    }
+
+    const updatetag = await database.tag.update({
+        where: {
+            id: id,
+        },
+        data: req.body
+    })
+
+    res.json({
+        message: 'tag updated',
+    });
+});
+
+router.get('/', async function (req, res, next) {
+    const { id } = req.query;
+    if (!id) {
+        res.status(400);
+        throw new Error('You must provide an id ');
+    }
+    const tags = await database.tag.findMany({
+        where: {
+            tagId: id,
+        },
+    })
+    res.json({
+        "tags": tags
+    });
+});
+
+module.exports = router;
