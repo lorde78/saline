49cab5e3353a12f319190890c49960149bb4172c
Bentani <amin.bentani@hetic.net>
2023-09-03 12:46:47 +0200

    fonc: méthode  PUT  video

--- backend/routes/video.routes.ts
+++ backend/routes/video.routes.ts
@@ -31,6 +31,26 @@ router.delete('/', async function (req, res, next) {
     });
 });
 
+router.put('/', async function (req, res, next) {
+    const { id } = req.query;
+
+    if (!id) {
+        res.status(400);
+        throw new Error('You must provide an id or lessonId.');
+    }
+
+    const updateVideo = await database.video.update({
+        where: {
+            id: id,
+        },
+        data: req.body
+    })
+
+    res.json({
+        message: 'video updated',
+    });
+});
+
 router.get('/', async function (req, res, next) {
     const { id, lessonId } = req.query;
     if (!id || !lessonId) {
