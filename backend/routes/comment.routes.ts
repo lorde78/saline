b0a082743d8217afd3f8d88276533f67373a845d
bentaniamin1 <amin.bentani@hetic.net>
2023-09-03 12:45:48 +0200

    fonc: méthode  PUT  comment

--- backend/routes/comment.routes.ts
+++ backend/routes/comment.routes.ts
@@ -33,6 +33,26 @@ router.delete('/', async function (req, res, next) {
     });
 });
 
+router.put('/', async function (req, res, next) {
+    const { id } = req.query;
+
+    if (!id) {
+        res.status(400);
+        throw new Error('You must provide an id or lessonId.');
+    }
+
+    const updateComment = await database.comment.update({
+        where: {
+            id: id,
+        },
+        data: req.body
+    })
+
+    res.json({
+        message: 'comment updated',
+    });
+});
+
 router.get('/', async function (req, res, next) {
     const { id, lessonId } = req.query;
     if (!id || !lessonId) {
