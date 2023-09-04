67a63521da9f14fae506d743c3cc6ecfbae47899
ArkhayDs <raphael.ds.tic@gmail.com>
2023-09-04 18:10:26 +0200

    fonc: ajout du hook useLogin et mise à jour du formulaire de login. Todo: ajout du register + gestion du type d'accès (admin vs user)

--- frontend/app/components/form_login.tsx
+++ frontend/app/components/form_login.tsx
@@ -1,18 +1,26 @@
 import Input from "~/kits/input";
 import {useState} from "react";
+import useLogin from "~/hook/useLogin";
 
 export default function Form_login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
+    const login = useLogin()
+
+    const submit = (e:any) => {
+        e.preventDefault()
+        //login(email,password)
+        console.log("hey")
+    }
 
     return (
-        <form className={"authentication_form_container"} action="" method="post">
+        <form className={"authentication_form_container"}>
             <Input name={"email"} type={"email"} placeholder={"Mail"}
                    setValue={setEmail} propsSetValue={""} value={email}/>
             <Input name={"password"} type={"password"} placeholder={"Mot de passe"}
                    setValue={setPassword} propsSetValue={""} value={password}/>
-            <button className={"button"} type="submit">Connexion</button>
+            <button className={"button"} type="submit" onClick={(e) => submit(e)}>Connexion</button>
             <a href={""} className={"sub_link"}>Tu as oublié ton mot de passe ?</a>
         </form>
     )
