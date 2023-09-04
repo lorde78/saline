f9c80111f227bb04f8a2a5bbdcaf440e4f3737af
bentaniamin1 <amin.bentani@hetic.net>
2023-09-04 19:28:33 +0200

    init: test unitaires

--- backend/__test__/test_routes/user.test.js
+++ backend/__test__/test_routes/user.test.js
@@ -1,21 +1,6 @@
 const app = require("../../app");
 const request = require('supertest');
 
-
-
-
-describe('Post Endpoints', () => {
-  it('should login user', async () => {
-    const res = await request(app)
-      .post('/login')
-      .send({
-        email: "test12@test.com",
-        password: "test12"
-      })
-      .expect(200)
-  });
-});
-
 describe('Put Endpoints', () => {
   it('should login user', async () => {
     const res = await request(app)
