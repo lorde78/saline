c6337ce67e62af88bf4115eb86cd6f29e49b6876
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-09-05 17:16:08 +0200

    init : création des pages classroom

---
+++ frontend/app/components/user_classroom.tsx
@@ -0,0 +1,42 @@
+import 'remixicon/fonts/remixicon.css'
+import Switch_button from "~/kits/switch_button";
+import {useState} from "react";
+import Input from "~/kits/input";
+import Color_picker from "~/kits/color_picker";
+import Checkbox from "~/kits/checkbox";
+import Textarea from "~/kits/textarea";
+import {NavLink, Outlet, useLocation} from "@remix-run/react";
+
+type Props = {
+    id: number,
+    title: string,
+    professor: string,
+    description: string
+    imgLink: string
+};
+export default function User_classroom({
+                                                 id,
+                                                 title,
+                                                 professor,
+                                                 description,
+                                                 imgLink,
+                                             }: Props) {
+
+
+    return (
+        <NavLink to={id.toString()} className={"backoffice_classroom_container"}>
+            <div className={"classroom_image"}>
+                <img src={imgLink} alt={"bannière du cour"}/>
+            </div>
+            <div className={"classroom_info"}>
+                <div className={"classroom_header_info"}>
+                    <p>{title}</p>
+                    <p>{professor}</p>
+                </div>
+                <p className={"classroom_description"}>
+                    {description}
+                </p>
+            </div>
+        </NavLink>
+    );
+}
