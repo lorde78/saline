5c06ccc1d4c073cc9b26a0dd69ca101a9fa7b3f6
lorde78 <basil.lombard@hetic.net>
2023-09-11 21:19:11 +0200

    update : few css update

--- frontend/app/routes/administration.tsx
+++ frontend/app/routes/administration.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import TagList from "../components/taglist";
 import administration from "~/styles/administration.css";
 
+export function links() {
+    return [
+        { rel: 'stylesheet', href: administration }
+    ]
+}
+
 export default function Administration() {
     return (
-        <div>
+        <div className="administration_container">
             <h1>Administration</h1>
             <div>Bienvenue dans la partie administration des labels</div>
             <TagList />
