bfbb17e8f14d91668820e143630cee6e595d63cd
ArkhayDs <raphael.ds.tic@gmail.com>
2023-09-11 12:37:09 +0200

    fix: correction de l'erreur du call axios useCreateBuilderElement. Modification du format d'envoi de formData

--- frontend/app/hook/useCreateBuilderElement.tsx
+++ frontend/app/hook/useCreateBuilderElement.tsx
@@ -3,16 +3,14 @@ import { AxiosInstance } from "~/axios/axiosInstance";
 export default function useCreateBuilderElement() {
     return (formData:any,creation_type:string) => {
         return AxiosInstance({
-            url: '/'+creation_type,
+            url: `/${creation_type}`,
             method: 'post',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
-            data: new URLSearchParams({
-                formData
-            })
+            data: new URLSearchParams(formData).toString()
         })
-            .then(res => console.log(res))
+            .then(res => res.data[creation_type])
             .catch(err => console.log(err))
     }
 }
