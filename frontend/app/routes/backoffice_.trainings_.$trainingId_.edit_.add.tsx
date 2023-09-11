1a9e85c58f128857fece57e53bba0d59f0a491b1
ArkhayDs <raphael.ds.tic@gmail.com>
2023-09-11 17:26:48 +0200

    fix: mise à jour des routes fronts /edit et /edit/add pour l'affichage dynamique des cours présents ou manquants aux parcours

--- frontend/app/routes/backoffice_.trainings_.$trainingId_.edit_.add.tsx
+++ frontend/app/routes/backoffice_.trainings_.$trainingId_.edit_.add.tsx
@@ -9,6 +9,9 @@ import Backoffice_edit_training from "~/components/backoffice_edit_training";
 import Checkbox from "~/kits/checkbox";
 import { useGlobalEffect } from "~/helper/globalHelper";
 import useGetAllElements from "~/hook/useGetAllElements";
+import getIdFromUrl from "~/helper/getIdFromUrl";
+import useGetCurrentElement from "~/hook/useGetCurrentElement";
+import Loader from "~/kits/loader";
 
 
 export function links() {
@@ -22,9 +25,20 @@ export function links() {
 
 export default function Backoffice_Trainings_TrainingId_Edit_Add() {
     useGlobalEffect()
+    const getCurrentId = getIdFromUrl(2)
+    const [loader,setLoader] = useState(false)
+
+    const [training, setTraining] = useState()
+    const getCurrentTraining = useGetCurrentElement()
 
     const [courses, setCourses] = useState([])
     const getAllCourses = useGetAllElements()
+
+    const getTraining = async () => {
+        const currentTraining = await getCurrentTraining("training",getCurrentId)
+        setTraining(currentTraining)
+        setLoader(true)
+    }
 
     useEffect(() => {
         getAllCourses("lesson").then(r => {
@@ -32,6 +46,8 @@ export default function Backoffice_Trainings_TrainingId_Edit_Add() {
                 setCourses(r)
             }
         })
+
+        getTraining()
     }, [])
 
     const [coursesAdd, setCoursesAdd] = useState({})
@@ -50,41 +66,50 @@ export default function Backoffice_Trainings_TrainingId_Edit_Add() {
         }
         setCoursesAdd(newCoursesAdd)
     }
+
     return (
         <>
-            <Header_section_page numberUndoPage={1}  title={"Ajouter un cour"}/>
-            <section className={"max_width_container"}>
-                <div className={"backoffice_training_preview_container max_width"}>
-                    <button className={"button"}>Ajouter les cours</button>
-                    {
-                        courses.map((course) => {
-                            let id = course.id
-                            return (
-                                <div className={"course_preview_container"}>
-                                    <Checkbox
-                                        name={"checkbox"}
-                                        type={"checkbox"}
-                                        text={""}
-                                        setValue={checkCourses}
-                                        propsSetValue={{id: id}}
-                                        // @ts-ignore
-                                        value={coursesAdd[id] ? coursesAdd[id].value : false}
-                                    />
-                                    <Backoffice_edit_training
-                                        id={course.id}
-                                        title={course.title}
-                                        author={course.author}
-                                        imgLink={course.bannerPicture}
-                                        description={course.description}
-                                        showButton={false}
-                                    />
-                                </div>
-                            )
-                        })
-                    }
-                    <button className={"button"}>Ajouter les cours</button>
-                </div>
-            </section>
+            {loader ?
+                <>
+                    <Header_section_page numberUndoPage={1}  title={"Ajouter un cours"}/>
+                    <section className={"max_width_container"}>
+                        <div className={"backoffice_training_preview_container max_width"}>
+                            <button className={"button"}>Ajouter les cours</button>
+                            {
+                                courses.filter(course => {
+                                    return course.trainings.length !== 0 || course.trainings.some(training => training.id !== training.id)
+                                }).map((course) => {
+                                    let id = course.id
+                                    return (
+                                        <div className={"course_preview_container"}>
+                                            <Checkbox
+                                                name={"checkbox"}
+                                                type={"checkbox"}
+                                                text={""}
+                                                setValue={checkCourses}
+                                                propsSetValue={{id: id}}
+                                                // @ts-ignore
+                                                value={coursesAdd[id] ? coursesAdd[id].value : false}
+                                            />
+                                            <Backoffice_edit_training
+                                                id={course.id}
+                                                title={course.title}
+                                                author={course.author}
+                                                imgLink={course.bannerPicture}
+                                                description={course.description}
+                                                showButton={false}
+                                            />
+                                        </div>
+                                    )
+                                })
+                            }
+                            <button className={"button"}>Ajouter les cours</button>
+                        </div>
+                    </section>
+                </>
+                :
+                <Loader/>
+            }
         </>
     )
 }
