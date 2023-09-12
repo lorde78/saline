1b044dba626a7d7a5da0f76dcc5c177c922798e0
ArkhayDs <raphael.ds.tic@gmail.com>
2023-09-12 17:50:35 +0200

    format: Début du refacto et typage typescript Remix authentication

--- frontend/app/routes/authentication.tsx
+++ frontend/app/routes/authentication.tsx
@@ -1,5 +1,5 @@
-import type {V2_MetaFunction} from "@remix-run/node";
-import {NavLink, Outlet, useLocation} from "@remix-run/react";
+import type { V2_MetaFunction } from "@remix-run/node";
+import { NavLink, Outlet, useLocation } from "@remix-run/react";
 
 import resetStyles from "~/styles/reset.css";
 import styles from "~/styles/style.css";
@@ -7,39 +7,50 @@ import input from "~/styles/input.css";
 import authentication from "~/styles/authentication.css";
 import Header_section_page from "~/kits/header_section_page";
 import { useGlobalEffect } from "~/helper/globalHelper";
+import { CSSProperties } from "react";
 
+type NavLinkProps = {
+    className?: string;
+    to: string;
+    style?: CSSProperties;
+};
 
 export const meta: V2_MetaFunction = () => {
     return [
-        {title: "New Remix App"},
-        {name: "description", content: "Welcome to Remix!"},
+        { title: "New Remix App" },
+        { name: "description", content: "Welcome to Remix!" },
     ];
 };
 
 export function links() {
     return [
-        {rel: 'stylesheet', href: resetStyles},
-        {rel: 'stylesheet', href: styles},
-        {rel: 'stylesheet', href: input},
-        {rel: 'stylesheet', href: authentication}
-    ]
+        { rel: "stylesheet", href: resetStyles },
+        { rel: "stylesheet", href: styles },
+        { rel: "stylesheet", href: input },
+        { rel: "stylesheet", href: authentication },
+    ];
 }
 
 export default function Authentication() {
-    useGlobalEffect()
+    useGlobalEffect();
 
     return (
         <div className={"authentication_container"}>
-            <NavLink className={"image_authentication"} to={"/"} >
-                <img  src={"https://www.salineacademy.com/wp-content/uploads/2022/12/627e08e5d58166bb632daf54_logo_sra_black.svg"} />
+            <NavLink className={"image_authentication"} to={"/"} style={{}}>
+                <img
+                    src={
+                        "https://www.salineacademy.com/wp-content/uploads/2022/12/627e08e5d58166bb632daf54_logo_sra_black.svg"
+                    }
+                    style={{}} // Ajoutez un style vide ou personnalisé si nécessaire
+                />
             </NavLink>
             <section className={"max_width_container"}>
                 <div className={"authentication_container-home max_width"}>
                     <div>
-                        <NavLink className={"button"} to={"register"}>
+                        <NavLink className={"button"} to={"register"} style={{}}>
                             Inscription
                         </NavLink>
-                        <NavLink className={"button button_dark"} to={"login"}>
+                        <NavLink className={"button button_dark"} to={"login"} style={{}}>
                             Connexion
                         </NavLink>
                     </div>
