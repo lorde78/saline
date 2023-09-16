3e9dea9c323f9697837b4a7b92b4f96fb87f2538
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-09-16 11:59:14 +0200

    fonc : protection du BO contre le role_user

--- frontend/app/helper/isLogged.tsx
+++ frontend/app/helper/isLogged.tsx
@@ -1,15 +1,25 @@
 import {useNavigate} from "react-router-dom";
 import {useContext, useEffect} from "react";
 import {signinContext} from "~/context/signinContext";
+import useGetCurrentUserRoles from "~/hook/useGetCurrentRoles";
 
-export function isLogged(isFrom:string) {
+export function isLogged(isFrom: string) {
     const navigate = useNavigate()
     // @ts-ignore
     const [signin, setSignin] = useContext(signinContext);
+    const getcurrentUserRoles = () => {
+        try {
+            if (signin) {
+                return useGetCurrentUserRoles(signin)
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
     useEffect(() => {
         if (!signin) {
-            switch(isFrom) {
+            switch (isFrom) {
                 case 'backoffice':
                     navigate('/backoffice/login');
                     break;
@@ -17,6 +27,14 @@ export function isLogged(isFrom:string) {
                 case 'user':
                     navigate('/authentication');
                     break;
+            }
+        } else {
+            if (isFrom === 'backoffice') {
+                const currentUserRoles = getcurrentUserRoles()
+                console.log(currentUserRoles)
+                if (currentUserRoles.length === 1) {
+                    navigate("/")
+                }
             }
         }
     }, [signin]);
