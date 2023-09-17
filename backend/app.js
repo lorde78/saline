62c44b65b7b9ae9f0c6790e5f2f65b3f83598686
ArkhayDs <raphael.ds.tic@gmail.com>
2023-09-17 14:51:37 +0200

    fix: access-control-allow-origin mauvais port renseigné

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ var app = express();
 app.use((req,res,next) => {
     res.setHeader(
         "Access-Control-Allow-Origin",
-        `${process.env.BASE_URL}`
+        `${process.env.BASE_URL}:3001`
     );
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
